401d2f31700bf68664804ea59f0d8e1354566c2f
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-07-30 22:35:51 -0500

    Removed duplicate package.json

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.static('public'));
 
 // Create Express.js routes for default '/', '/send' and '/routes' endpoints
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
 );
 
 app.get('/api/db', (req, res) => res.json(database));
