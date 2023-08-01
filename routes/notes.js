fb3f7fdddecfa62977fd209017e95a3c790ffc94
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-08-01 00:47:59 -0500

    Created Routes folder to organize route information

---
+++ routes/notes.js
@@ -0,0 +1,6 @@
+const notes = require('express').Router();
+
+// GET Route for retrieving all the feedback
+notes.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
